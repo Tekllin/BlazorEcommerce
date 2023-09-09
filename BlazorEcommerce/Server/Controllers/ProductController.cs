40a4748ce59e9b59cb3e4e288d4aa7394fa909f4
Tekllin <111495074+Tekllin@users.noreply.github.com>
2023-09-09 19:12:03 +0200

    Ajoutez des fichiers projet.

---
+++ BlazorEcommerce/Server/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using BlazorEcommerce.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BlazorEcommerce.Server.Data;
+
+namespace BlazorEcommerce.Server.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductController : ControllerBase
+	{
+		private readonly DataContext _context;
+
+		public ProductController(DataContext context)
+		{
+			_context = context;
+		}
+		[HttpGet]
+		public List<Product> GetProduct()
+		{
+			var products = _context.Products.ToList();
+			return products;
+		}
+	}
+}
+
